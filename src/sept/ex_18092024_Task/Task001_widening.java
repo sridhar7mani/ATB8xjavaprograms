3ba2a0c89e1fe59c40dd6c90f30a3a3dbc801592
sridhar mani <mail4sri711@gmail.com>
2024-09-18 22:47:22 +0530

    All the Task files 18sept

---
+++ src/sept/ex_18092024_Task/Task001_widening.java
@@ -0,0 +1,19 @@
+package sept.ex_18092024_Task;
+
+public class Task001_widening {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 15000;
+        //long c = a + b; // widening _implict casting
+        long c = (long)(a + b);//widening _explicit casting
+        System.out.println(c);
+
+
+        long s = 98564321l;
+        float m = s;//widening _implicit casting
+        float k = (float)s;//Widening _explicit casting
+        System.out.println(m);
+        System.out.println(k);
+
+    }
+}
---
+++ src/sept/ex_18092024_Task/Task003.java
@@ -0,0 +1,9 @@
+package sept.ex_18092024_Task;
+
+public class Task003 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println( --a + a++ + a--);
+        System.out.println(a);
+    }
+}
